642c9ef6a12afe99b98e790a67fd4e9e50ec07c3
Jaffar <ajaffar@uh.edu>
2020-07-03 18:05:59 -0500

    Update models.py
    make sure to run python manage.py makemigrations and then python manage.py migrate

--- Fuel_Quote/quote/models.py
+++ Fuel_Quote/quote/models.py
@@ -6,8 +6,11 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
 	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
-	phone = models.CharField(max_length=200, null=True)
-	email = models.CharField(max_length=200, null=True)
+	Address_1 = models.CharField(max_length=200, null=True)
+	Address_2 = models.CharField(max_length=200, null=True)
+	City = models.CharField(max_length=200, null=True)
+	State = models.CharField(max_length=200, null=True)
+	Zip_Code = models.CharField(max_length=200, null=True)
 	profile_pic = models.ImageField(default="profile1.png", null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
