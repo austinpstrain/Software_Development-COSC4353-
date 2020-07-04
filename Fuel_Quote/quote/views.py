781a37e98b52e230f89bb6da25d5c5b94948ea5a
austinpstrain <47685531+austinpstrain@users.noreply.github.com>
2020-07-04 15:54:31 -0500

    fixed bug after pull

--- Fuel_Quote/quote/views.py
+++ Fuel_Quote/quote/views.py
@@ -23,7 +23,7 @@ def history(request, pk):
 	quotes = customer.quote_set.all() 
 	context = {'customer' : customer, 'quotes' : quotes}
 	return render(request, "quote/history.html", context) """
-
+@login_required(login_url='login')
 def home(request, pk):
 	customer = Customer.objects.get(id=pk)
 	quotes = customer.quote_set.all() 
@@ -31,59 +31,58 @@ def home(request, pk):
 	return render(request, 'quote/home.html', context)
 
 
-
+@login_required(login_url='login')
 def profileManager(request):
 
-	form = CreateUserForm()
-	if request.method == 'POST':
-		form = CreateUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			username = form.cleaned_data.get('username')
-			messages.success(request, 'Profile was created for ' + username)
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
 
-			return redirect('home')
-		
+            messages.success(request, 'Profile was created for ' + username)
 
-	context = {'form':form}
-	return render(request, 'quote/profileManager.html', context)
+            return redirect('home')
 
+    context = {'form': form}
+    return render(request, 'quote/home.html', context)
 
+@unauthenticated_user
 def registerClient(request):
 
-	form = CreateUserForm()
-	if request.method == 'POST':
-		form = CreateUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			username = form.cleaned_data.get('username')
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
 
+            messages.success(request, 'Account was created for ' + username)
 
-			messages.success(request, 'Account was created for ' + username)
+            return redirect('login')
 
-			return redirect('login')
-		
-
-	context = {'form':form}
-	return render(request, 'quote/registerClient.html', context)
+    context = {'form': form}
+    return render(request, 'quote/registerClient.html', context)
 
 @unauthenticated_user
 def loginPage(request):
 
-	if request.method == 'POST':
-		username = request.POST.get('username')
-		password =request.POST.get('password')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-		user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=username, password=password)
 
-		if user is not None:
-			login(request, user)
-			return redirect('account')
-		else:
-			messages.info(request, 'Username OR password is incorrect')
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
 
-	context = {}
-	return render(request, 'quote/login.html', context)
+    context = {}
+    return render(request, 'quote/login.html', context)
+
 	
 
 
@@ -93,30 +92,30 @@ def logoutUser(request):
 
 
 
+@login_required(login_url='login')
 def accountSettings(request):
-	customer = request.user.customer
-	form = CustomerForm(instance=customer)
+    customer = request.user.customer
+    form = CustomerForm(instance=customer)
 
-	if request.method == 'POST':
-		form = CustomerForm(request.POST, request.FILES,instance=customer)
-		if form.is_valid():
-			form.save()
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'quote/account_settings.html', context)
 
 
-	context = {'form':form}
-	return render(request, 'quote/account_settings.html', context)
-
-	
+@login_required(login_url='login')
 def userPage(request):
-	context={}
-	return render(request, 'quote/user.html')
+    context = {}
+    return render(request, 'quote/user.html')
 
 
+@login_required(login_url='login')
 def customer(request):
-	
-	return render(request, 'quote/customer.html')
 
-
+    return render(request, 'quote/customer.html')
 
 
 
