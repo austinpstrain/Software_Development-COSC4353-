c17fdf54c966af69e692d7380e382ecde7bdeec5
austinpstrain <47685531+austinpstrain@users.noreply.github.com>
2020-07-04 13:44:06 -0500

    Added quote model and made quote and history view pull from DB

--- Fuel_Quote/quote/models.py
+++ Fuel_Quote/quote/models.py
@@ -4,68 +4,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Customer(models.Model):
-
-	CATEGORY = (
-			('AL', 'Alabama'),
-			('AK', 'Alaska'),
-			('AZ', 'Arizona'),
-			('AR', 'Arkansan'),
-			('CA', 'California'),
-			('CO', 'Colorado'),
-			('CT', 'Connecticut'),
-			('DE', 'Delaware'),
-			('DC', 'District of Columbia'),
-			('FL', 'Florida'),
-			('GA', 'Georgia'),
-			('HI', 'Hawaii'),
-			('ID', 'Idaho'),
-			('IL', 'Illinois'),
-			('IN', 'Indiana'),
-			('IA', 'Iowa'),
-			('KS', 'Kansas'),
-			('KY', 'Kentucky'),
-			('LA', 'Louisiana'),
-			('ME', 'Maine'),
-			('MD', 'Maryland'),
-			('MA', 'Massachusetts'),
-			('MI', 'Michigan'),
-			('MN', 'Minnesota'),
-			('MS', 'Mississippi'),
-			('MO', 'Missouri'),
-			('MT', 'Montana'),
-			('NE', 'Nebraska'),
-			('NV', 'Nevada'),
-			('NH', 'New Hampshire'),
-			('NJ', 'New Jersey'),
-			('NM', 'New Mexico'),
-			('NY', 'New York'),
-			('NC', 'North Carolina'),
-			('ND', 'North Dakota'),
-			('OH', 'Ohio'),
-			('OK', 'Oklahoma'),
-			('OR', 'Oregon'),
-			('PA', 'Pennsylvania'),
-			('RI', 'Rhode Island'),
-			('SC', 'South Carolina'),
-			('SD', 'South Dakota'),
-			('TN', 'Tennessee'),
-			('TX', 'Texas'),
-			('UT', 'Utah'),
-			('VT', 'Vermont'),
-			('VA', 'Virginia'),
-			('WA', 'Washington'),
-			('WV', 'West Virginia'),
-			('WI', 'Wisconsin'),
-			('WY', 'Wyoming'),
-
-			) 
 	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-	name = models.CharField(max_length=50, null=True)
-	Address_1 = models.CharField(max_length=100, null=True)
-	Address_2 = models.CharField(max_length=100, null=True)
-	City = models.CharField(max_length=100, null=True)
-	State = models.CharField(max_length=100, null=True, choices=CATEGORY)
-	Zip_Code = models.CharField(max_length=10, null=True)
+	name = models.CharField(max_length=200, null=True)
+	phone = models.CharField(max_length=200, null=True)
+	email = models.CharField(max_length=200, null=True)
 	profile_pic = models.ImageField(default="profile1.png", null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
@@ -79,6 +21,5 @@ class Quote(models.Model):
 	gallonsRequested = models.FloatField()
 	Total = models.FloatField()
 
-class History(models.Model):
-	#quote = 
-	
+"""#class History(models.Model):
+	#quote = """
